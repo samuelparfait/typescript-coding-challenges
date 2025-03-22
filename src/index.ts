67131bda86bf03c444dae84d3776665bfdd93d05
Samuel Parfait <58629989+samuelparfait@users.noreply.github.com>
2025-03-22 22:05:45 +0000

    chore: initial

--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,30 @@ console.log(`
     ──└──────────────────────────────────────────────┘──
     ────────────────────────────────────────────────────
   `);
+
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/webhook', (request, response) => {
+  response.status(202).send('Accepted');
+
+  const githubEvent = request.headers['x-github-event'];
+
+  if (githubEvent === 'push') {
+    const data = request.body;
+    const action = data.action;
+
+    console.log({ action, data });
+  } else {
+    console.log(`Unhandled event: ${githubEvent}`);
+  }
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});

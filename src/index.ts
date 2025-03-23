b93ee3f8b2ead02188252f4ce7534339d2e20452
Samuel Parfait <58629989+samuelparfait@users.noreply.github.com>
2025-03-23 11:29:31 +0000

    updated script

--- src/index.ts
+++ src/index.ts
@@ -19,17 +19,17 @@ const app = express();
 app.use(express.json());
 app.use(cors({ origin: ['https://bee7-149-107-71-118.ngrok-free.app'] }));
 
-app.get('/api/webhook', (request, response) => {
-  response.status(200).send('Ok');
+app.get('/api/webhook', (req, res) => {
+  res.status(200).send('Ok');
 });
 
-app.post('/api/webhook', (request, response) => {
-  response.status(202).send('Accepted');
+app.post('/api/webhook', (req, res) => {
+  res.status(202).send('Accepted');
 
-  const githubEvent = request.headers['x-github-event'];
+  const githubEvent = req.headers['x-github-event'];
 
   if (githubEvent === 'push') {
-    const data = request.body;
+    const data = req.body;
     const action = data.action;
 
     console.log({ action, data });
